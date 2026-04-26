6288da4b21b85c4ee6be4e8e279d85dd96fa86c3
idanilchenko@altsoft.biz <idanilchenko@altsoft.biz>
2026-04-26 10:00:24 +0300

    add file b tree

---
+++ lib/v1/src/main/java/ru/otus/btree/v1/FileBTree.java
@@ -0,0 +1,24 @@
+package ru.otus.btree.v1;
+
+import ru.otus.btree.lib.api.btree.IBTree;
+import ru.otus.btree.lib.api.btree.IEntity;
+import ru.otus.btree.lib.api.btree.Element;
+
+public class FileBTree implements IBTree {
+
+    @Override
+    public void insert(String keyName, IEntity entity) {
+        // TODO: Implement insert
+    }
+
+    @Override
+    public IEntity search(Element element) {
+        // TODO: Implement search
+        return null;
+    }
+
+    @Override
+    public void delete(Element element) {
+        // TODO: Implement delete
+    }
+}
