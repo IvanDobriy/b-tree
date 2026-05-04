aacce415181633a86f08f4060bb2ba24b20e2b31
idanilchenko@altsoft.biz <idanilchenko@altsoft.biz>
2026-05-04 08:23:10 +0300

    add interface

---
+++ lib/api/src/main/java/ru/otus/btree/lib/api/storage/IStorage.java
@@ -0,0 +1,18 @@
+package ru.otus.btree.lib.api.storage;
+
+import ru.otus.btree.lib.api.array.IArray;
+import ru.otus.btree.lib.api.btree.Element;
+import ru.otus.btree.lib.api.btree.IEntity;
+
+
+public interface IStorage {
+    IArray<Result> get(Element element);
+
+    IArray<Result> get(long from, long to);
+
+    void insert(IArray<IEntity> entity);
+
+    void remove(Element element);
+
+    long size();
+}
---
+++ lib/api/src/main/java/ru/otus/btree/lib/api/storage/Result.java
@@ -0,0 +1,8 @@
+package ru.otus.btree.lib.api.storage;
+
+import ru.otus.btree.lib.api.btree.IEntity;
+
+public class Result {
+    private IEntity data;
+    private long position;
+}
