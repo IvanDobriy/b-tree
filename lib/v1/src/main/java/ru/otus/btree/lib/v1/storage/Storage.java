eb90348291ff9b0244be24e1bdba7508fe316960
idanilchenko@altsoft.biz <idanilchenko@altsoft.biz>
2026-05-04 20:17:36 +0300

    preapare storage

---
+++ lib/v1/src/main/java/ru/otus/btree/lib/v1/storage/Storage.java
@@ -0,0 +1,54 @@
+package ru.otus.btree.lib.v1.storage;
+
+import ru.otus.btree.lib.api.array.IArray;
+import ru.otus.btree.lib.api.btree.Element;
+import ru.otus.btree.lib.api.btree.IEntity;
+import ru.otus.btree.lib.api.storage.IStorage;
+import ru.otus.btree.lib.api.storage.Result;
+import ru.otus.btree.lib.v1.array.SingleArray;
+import ru.otus.btree.lib.v1.btree.FileBTreeUtils;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.Objects;
+
+public class Storage implements IStorage {
+    private final FileChannel dataChannel;
+    private final StorageManager storageManager;
+    private long size;
+
+    public Storage(FileChannel dataChannel, FileChannel metaChannel) {
+        this.dataChannel = Objects.requireNonNull(dataChannel, "data channel is null");
+        this.storageManager = new StorageManager(Objects.requireNonNull(metaChannel, "meta channel is null"));
+        this.size = 0;
+    }
+
+    @Override
+    public IArray<Result> get(Element element) {
+        // TODO: implement indexed search
+        return new SingleArray<>(0);
+    }
+
+    @Override
+    public IArray<Result> get(long from, long to) {
+        // TODO: implement range scan
+        return new SingleArray<>(0);
+    }
+
+    @Override
+    public void insert(IArray<IEntity> entity) {
+        Objects.requireNonNull(entity, "entity array is null");
+    }
+
+    @Override
+    public void remove(Element element) {
+        throw new RuntimeException("not yet implemented");
+    }
+
+    @Override
+    public long size() {
+        return size;
+    }
+
+}
