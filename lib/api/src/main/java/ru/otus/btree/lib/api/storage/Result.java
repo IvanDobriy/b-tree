4973b0a01518da69cdfd1dcbd5a1da2f75275958
idanilchenko@altsoft.biz <idanilchenko@altsoft.biz>
2026-05-04 20:17:50 +0300

    prepare result

--- lib/api/src/main/java/ru/otus/btree/lib/api/storage/Result.java
+++ lib/api/src/main/java/ru/otus/btree/lib/api/storage/Result.java
@@ -5,4 +5,17 @@ import ru.otus.btree.lib.api.btree.IEntity;
 public class Result {
     private IEntity data;
     private long position;
+
+    public Result(IEntity data, long position) {
+        this.data = data;
+        this.position = position;
+    }
+
+    public IEntity getData() {
+        return data;
+    }
+
+    public long getPosition() {
+        return position;
+    }
 }
