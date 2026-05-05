2c5c93e269c547acce39b0dcf19cca7e161a72ac
idanilchenko@altsoft.biz <idanilchenko@altsoft.biz>
2026-05-05 08:07:48 +0300

    add IStorageIndexInfo

---
+++ data/src/main/java/ru/otus/btree/data/StorageIndexInfo.java
@@ -0,0 +1,30 @@
+package ru.otus.btree.data;
+
+import ru.otus.btree.domain.IStorageIndexInfo;
+
+public class StorageIndexInfo implements IStorageIndexInfo {
+    private final String fieldName;
+    private final long size;
+    private final long fileSize;
+
+    public StorageIndexInfo(String fieldName, long size, long fileSize) {
+        this.fieldName = fieldName;
+        this.size = size;
+        this.fileSize = fileSize;
+    }
+
+    @Override
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    @Override
+    public long getSize() {
+        return size;
+    }
+
+    @Override
+    public long getFileSize() {
+        return fileSize;
+    }
+}
