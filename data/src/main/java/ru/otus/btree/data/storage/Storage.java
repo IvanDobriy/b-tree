7534126f89d4fb55e428c67e85ed45b5154c57e2
idanilchenko@altsoft.biz <idanilchenko@altsoft.biz>
2026-05-05 09:50:54 +0300

    fix interface IStorage

--- data/src/main/java/ru/otus/btree/data/storage/Storage.java
+++ data/src/main/java/ru/otus/btree/data/storage/Storage.java
@@ -4,9 +4,12 @@ import ru.otus.btree.domain.IStorage;
 import ru.otus.btree.domain.IStorageEntityInfo;
 import ru.otus.btree.domain.IStorageIndexInfo;
 import ru.otus.btree.lib.api.array.IArray;
+import ru.otus.btree.lib.api.btree.Element;
+import ru.otus.btree.lib.api.btree.IEntity;
 import ru.otus.btree.lib.v1.array.SingleArray;
 
 public class Storage implements IStorage {
+    private StorageEntityList storageEntityList;
 
     @Override
     public void createIndex(String entityName, String fieldName) {
@@ -16,6 +19,21 @@ public class Storage implements IStorage {
     @Override
     public IArray<IStorageIndexInfo> getIndexInfo(String entityName) {
         return new SingleArray<>(0);
+    }
+
+    @Override
+    public void createEntityStorage(String name) {
+
+    }
+
+    @Override
+    public void setEntity(IEntity entity, String name) {
+
+    }
+
+    @Override
+    public IArray<IEntity> getEntity(Element element) {
+        return null;
     }
 
     @Override
--- domain/src/main/java/ru/otus/btree/domain/IStorage.java
+++ domain/src/main/java/ru/otus/btree/domain/IStorage.java
@@ -1,13 +1,17 @@
 package ru.otus.btree.domain;
 
 import ru.otus.btree.lib.api.array.IArray;
+import ru.otus.btree.lib.api.btree.Element;
+import ru.otus.btree.lib.api.btree.IEntity;
 
 public interface IStorage {
     void createIndex(String entityName, String fieldName);
-
     IArray<IStorageIndexInfo> getIndexInfo(String entityName);
 
-    IArray<String> getEntitiesList();
+    void createEntityStorage(String name);
+    void setEntity(IEntity entity, String name);
+    IArray<IEntity> getEntity(Element element);
 
+    IArray<String> getEntitiesList();
     IStorageEntityInfo getEntityInfo(String name);
 }
